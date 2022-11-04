9847422c563ad808c56572050c0184781c7c89ee
maaxayotte <maxayotte834@gmail.com>
2022-11-04 13:30:50 -0400

    button stuff

--- stories/components/buttons/ConnectButton.tsx
+++ stories/components/buttons/ConnectButton.tsx
@@ -4,23 +4,35 @@
  * @author max <max@mirage.space> | November 3, 2022 | Updated: nevah
  * ----------------------------------------------------------------------------------*/
 
-import React from 'react';
-import './connectButton.css';
+import React from "react";
+import "./connectButton.css";
 
 interface ConnectButtonProps {
-    primary?: boolean;
-    enabled?: boolean;
-    loading?: boolean;
-    size: "large" | "small";
-    label: string;
+  primary: boolean;
+  enabled: boolean;
+  loading: boolean;
+  size: "large" | "small";
+  label: string;
 }
 
-export const ConnectButton = ({ label, primary=true, size="large", loading=false }: ConnectButtonProps) => {
-    const mode = primary ? 'button--primary' : 'button--secondary';
-    const isLoading = loading ? '--loading' : ''
-    return (
-        <button className={['button', `button--${size}`, mode, isLoading].join(' ')}>
-            {isLoading ? <img src={"/gifs/spinner.gif"} /> : label}
-        </button>
-    );
-}
+export const ConnectButton = ({
+  label,
+  primary = true,
+  size = "large",
+  loading = false,
+  enabled = true,
+}: ConnectButtonProps) => {
+  const mode = primary ? "button--primary" : "button--secondary";
+  const isLoading = loading ? "--loading" : "";
+  const isEnabled = !enabled ? "--disabled" : "";
+
+  return (
+    <button
+      className={["button", `button--${size}`, mode, isLoading, isEnabled].join(
+        " "
+      )}
+    >
+      {isLoading ? <img src={"/gifs/spinner.gif"} /> : label}
+    </button>
+  );
+};
